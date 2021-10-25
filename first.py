255bf707c9b482b52052587c0218341210eab1ab
Ivan <ivan.markov.2013@gmail.com>
2021-10-25 15:38:26 +0300

    changed output

--- first.py
+++ first.py
@@ -6,7 +6,7 @@ from sympy import *
 def first_task():
     number = random.randint(int(1e4), int(1e5)) / pow(10, random.randint(1, 3))
     epsilon = random.randint(1, 9) / pow(10, random.randint(0, 3))
-    print('Дано приближенное число {}, погрешность - {}. Найти число верных знаков.'
+    print('Задание - Дано приближенное число {}, погрешность - {}. Найти число верных знаков.'
           .format(number, epsilon))
 
     m = 0
@@ -16,7 +16,7 @@ def first_task():
     n = 0
     while epsilon <= 0.5 * pow(10, m - n + 1):
         n += 1
-    print('Кол-во верных знаков = {}.'
+    print('Ответ: Кол-во верных знаков = {}.'
           .format(n - 1))
 
 
@@ -35,71 +35,73 @@ def second_task():
 
 
 def trapeze():
-    a = random.randint(10, 40)
-    b = random.randint(a + 20, 90)
-    h = random.randint(10, 100)
-    print('Равнобедренная трапеция со сторонами основания, равными {} и {} см, и высотой, равной {} см.'
+    beginning = random.randint(2, 8)
+    a = beginning * 5
+    b = random.randint(beginning + 2, 18) * 5
+    h = random.randint(2, 20) * 5
+    print('Задание - Равнобедренная трапеция со сторонами основания, равными {} и {} см, и высотой, равной {} см.'
           .format(a, b, h))
 
     s = (a + b) / 2 * h
     abs_s = h + (a + b) / 2
     rel_s = (abs_s / s) * 100
-    print('Площадь фигуры = {} +- {} см^2, относительная погрешность = {} %'
-          .format(s, abs_s, rel_s))
+    print('Ответ: Площадь фигуры = {} +- {} см^2, относительная погрешность = {} %'
+          .format(round(s, 3), round(abs_s, 3), round(rel_s, 3)))
 
     p = 2 * math.sqrt(h ** 2 + ((b - a) / 2) ** 2) + a + b
     abs_p = 4 * h / math.sqrt(4 * h ** 2 + b ** 2 - 2 * a * b + a ** 2) + \
             (a - b + 2) / math.sqrt(4 * h ** 2 + b ** 2 - 2 * a * b + a ** 2 + 4 * (a + b)) * 2
     rel_p = (abs_p / p) * 100
     print('Периметр = {} +- {} см, относительная погрешность = {} %'
-          .format(p, abs_p, rel_p))
+          .format(round(p, 3), round(abs_p, 3), round(rel_p, 3)))
 
 
 def pyramid():
-    a = random.randint(10, 150)
-    h = random.randint(10, 100)
-    print('Правильная четырехугольная пирамида со стороной основания, равной {} см, и высотой, равной {} см.'
+    a = random.randint(2, 30) * 5
+    h = random.randint(2, 20) * 5
+    print('Задание - Правильная четырехугольная пирамида со стороной основания, равной {} см, и высотой, равной {} см.'
           .format(a, h))
 
     v = 1 / 3 * a ** 2 * h
     abs_v = 1 / 3 * a * (2 * h + a)
     rel_v = (abs_v / v) * 100
-    print('Обьем фигуры = {} +- {} см^3, относительная погрешность вычисления = {} %'
-          .format(v, abs_v, rel_v))
+    print('Ответ: Обьем фигуры = {} +- {} см^3, относительная погрешность вычисления = {} %'
+          .format(round(v, 3), round(abs_v, 3), round(rel_v, 3)))
 
     sup = math.sqrt(4 * h ** 2 + a ** 2)
     s = 2 * a * sup + a ** 2
     abs_s = 2 * sup + 2 * a ** 2 / sup + 2 * a + 8 * a * h / sup
     rel_s = (abs_s / s) * 100
     print('Площадь поверхности фигуры = {} +- {} см^2, относительная погрешность вычисления = {} %'
-          .format(s, abs_s, rel_s))
+          .format(round(s, 3), round(abs_s, 3), round(rel_s, 3)))
 
 
 def cone():
-    h = random.randint(10, 50)
-    r = random.randint(5, 50)
-    print('Конус с высотой, равной {} см, и радиусом, равным {} см.'
+    h = random.randint(2, 10) * 5
+    r = random.randint(1, 10) * 5
+    print('Задание - Конус с высотой, равной {} см, и радиусом, равным {} см.'
           .format(h, r))
 
     v = 1 / 3 * math.pi * r ** 2 * h
     abs_v = 1 / 3 * math.pi * r * (r + 2 * h)
     rel_v = (abs_v / v) * 100
-    print('Обьем фигуры = {} +- {} см^3, относительная погрешность вычисления = {} %'
-          .format(v, abs_v, rel_v))
+    print('Ответ: Обьем фигуры = {} +- {} см^3, относительная погрешность вычисления = {} %'
+          .format(round(v, 3), round(abs_v, 3), round(rel_v, 3)))
 
     sup = math.sqrt(h ** 2 + r ** 2)
     s = math.pi * r * (sup + r)
     abs_s = math.pi * sup + math.pi * r ** 2 / sup + math.pi * h * r / sup
     rel_s = (abs_s / s) * 100
     print('Площадь поверхности фигуры = {} +- {} см^2, относительная погрешность вычисления = {} %'
-          .format(s, abs_s, rel_s))
+          .format(round(s, 3), round(abs_s, 3), round(rel_s, 3)))
 
 
 def parallelepiped():
-    h = random.randint(10, 100)
-    a = random.randint(10, 100)
-    d = random.randint(a, 150)
-    print('Прямоугольный параллелепипед с высотой {} см, стороной основания {} см и диагональю основания {} см.'
+    h = random.randint(4, 20) * 5
+    beginning = random.randint(4, 20)
+    a = beginning * 5
+    d = random.randint(beginning, 30) * 5
+    print('Задание - Прямоугольный параллелепипед с высотой {} см, стороной основания {} см и диагональю основания {} см.'
           .format(h, a, d))
 
     sup = math.sqrt(d ** 2 - a ** 2)
@@ -107,33 +109,33 @@ def parallelepiped():
     v = a * sup * h
     abs_v = sup * h - a ** 2 / sup * h + a * d / sup * h + a * sup
     rel_v = (abs_v / v) * 100
-    print('Обьем фигуры = {} +- {} см^3, относительная погрешность вычисления = {} %'
-          .format(v, abs_v, rel_v))
+    print('Ответ: Обьем фигуры = {} +- {} см^3, относительная погрешность вычисления = {} %'
+          .format(round(v, 3), round(abs_v, 3), round(rel_v, 3)))
 
     s = 2 * ((a + h) * sup + a * h)
     abs_s = 2 * (sup - a ** 2 / sup - a * h / sup + h + (a + h) * d / sup + sup + a)
     rel_s = (abs_s / s) * 100
     print('Площадь поверхности фигуры = {} +- {} см^2, относительная погрешность вычисления = {} %'
-          .format(s, abs_s, rel_s))
+          .format(round(s, 3), round(abs_s, 3), round(rel_s, 3)))
 
 
 def cylinder():
-    h = random.randint(10, 100)
-    d = random.randint(10, 100)
-    print('Цилиндр с образующей, равной {} см, и главной диагональю, равной {} см.'
+    h = random.randint(4, 20) * 5
+    d = random.randint(4, 20) * 5
+    print('Задание - Цилиндр с образующей, равной {} см, и главной диагональю, равной {} см.'
           .format(h, d))
 
     v = (math.pi * (d ** 2) * h) / 4
     abs_v = (math.pi * d * h) / 2 + (math.pi * (d ** 2)) / 4
     rel_v = (abs_v / v) * 100
-    print('Обьем фигуры = {} +- {} см^3, относительная погрешность вычисления = {} %'
-          .format(v, abs_v, rel_v))
+    print('Ответ: Обьем фигуры = {} +- {} см^3, относительная погрешность вычисления = {} %'
+          .format(round(v, 3), round(abs_v, 3), round(rel_v, 3)))
 
     s = 2 * math.pi * (d / 2) * (d / 2 + h)
     abs_s = math.pi * (3 * d + h)
     rel_s = (abs_s / s) * 100
     print('Площадь поверхности фигуры = {} +- {} см^2, относительная погрешность вычисления = {} %'
-          .format(s, abs_s, rel_s))
+          .format(round(s, 3), round(abs_s, 3), round(rel_s, 3)))
 
 
 def third_task():
@@ -222,7 +224,7 @@ def third_task():
     dy_str = (str(dy)).replace('**', '^')
     dz = eval(str(diff(infix, z)))
     dz_str = (str(dz)).replace('**', '^')
-    print('Абсолютная погрешность функции = ({}) * del1 + ({}) * del2 + ({}) * del3'
+    print('Абсолютная погрешность функции = [{}] * del1 + [{}] * del2 + [{}] * del3'
           .format(dx_str, dy_str, dz_str))
 
     dx = eval('{} / {}'.format(dx, infix))
@@ -231,7 +233,7 @@ def third_task():
     dy_str = (str(dy)).replace('**', '^')
     dz = eval('{} / {}'.format(dz, infix))
     dz_str = (str(dz)).replace('**', '^')
-    print('Относительная погрешность функции = ({}) * del1 + ({}) * del2 + ({}) * del3'
+    print('Относительная погрешность функции = [{}] * del1 + [{}] * del2 + [{}] * del3'
           .format(dx_str, dy_str, dz_str))
 
 
